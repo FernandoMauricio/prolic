37b0067d79ed116609d6e95142f4020fdde19f01
Fernando Mauricio <fernando.mauricio@am.senac.br>
2025-05-02 12:54:09 -0400

    feat(Navbar): aprimorar estilo e responsividade da navbar com novos ícones e cores

--- views/layouts/navbar.php
+++ views/layouts/navbar.php
@@ -10,49 +10,49 @@ NavBar::begin([
     'brandLabel' => Html::img('@web/css/img/logo_senac_topo.png', ['alt' => 'Senac', 'height' => 40]),
     'brandUrl' => Yii::$app->homeUrl,
     'options' => [
-        'class' => 'navbar navbar-expand-lg navbar-dark bg-dark fixed-top',
+        'class' => 'navbar navbar-expand-lg navbar-dark navbar-custom fixed-top',
     ],
 ]);
 
 echo NavX::widget([
     'options' => ['class' => 'navbar-nav ms-auto'],
     'activateParents' => true,
+    'encodeLabels' => false, // permite usar ícones
     'items' => $session['sess_codunidade'] == 6 ? [
 
         [
-            'label' => 'Administração',
+            'label' => '<i class="bi bi-gear-fill me-1"></i> Administração',
             'items' => [
-                ['label' => 'Parâmetros', 'items' => [
-                    '<li class="dropdown-header">Administração dos Parâmetros</li>',
-                    ['label' => 'Ano', 'url' => ['/base/ano/index']],
-                    ['label' => 'Artigo', 'url' => ['/base/artigo/index']],
-                    ['label' => 'Empresa', 'url' => ['/base/empresa/index']],
-                    ['label' => 'Comprador', 'url' => ['/base/comprador/index']],
-                    ['label' => 'Modalidade', 'url' => ['/base/modalidade/index']],
-                    ['label' => 'Modalidade - Valor Limite', 'url' => ['/base/modalidade-valorlimite/index']],
-                    ['label' => 'Ramo', 'url' => ['/base/ramo/index']],
-                    ['label' => 'Recursos', 'url' => ['/base/recursos/index']],
+                ['label' => '<i class="bi bi-sliders"></i> <strong>Parâmetros</strong>', 'items' => [
+                    ['label' => '<i class="bi bi-calendar2-week"></i> Ano', 'url' => ['/base/ano/index']],
+                    ['label' => '<i class="bi bi-journal-text"></i> Artigo', 'url' => ['/base/artigo/index']],
+                    ['label' => '<i class="bi bi-buildings"></i> Empresa', 'url' => ['/base/empresa/index']],
+                    ['label' => '<i class="bi bi-person"></i> Comprador', 'url' => ['/base/comprador/index']],
+                    ['label' => '<i class="bi bi-tag-fill"></i> Modalidade', 'url' => ['/base/modalidade/index']],
+                    ['label' => '<i class="bi bi-tags"></i> Valor Limite', 'url' => ['/base/modalidade-valorlimite/index']],
+                    ['label' => '<i class="bi bi-diagram-2"></i> Ramo', 'url' => ['/base/ramo/index']],
+                    ['label' => '<i class="bi bi-lightning-charge-fill"></i> Recursos', 'url' => ['/base/recursos/index']],
                 ]],
                 '<li class="dropdown-divider"></li>',
-                '<li class="dropdown-header">Área do Administrador</li>',
-                ['label' => 'Processos Licitatórios', 'url' => ['/processolicitatorio/processo-licitatorio/index']],
-                ['label' => 'Agenda de Compromissos', 'url' => ['/processolicitatorio/agenda/index']],
-                ['label' => 'Acompanhamento dos Processos', 'url' => ['/processolicitatorio/dashboard/index']],
-            ]
+                '<li class="dropdown-header text-muted">Área do Administrador</li>',
+                ['label' => '<i class="bi bi-clipboard-data"></i> Processos Licitatórios', 'url' => ['/processolicitatorio/processo-licitatorio/index']],
+                ['label' => '<i class="bi bi-calendar3"></i> Agenda de Compromissos', 'url' => ['/processolicitatorio/agenda/index']],
+                ['label' => '<i class="bi bi-bar-chart-line-fill"></i> Acompanhamento', 'url' => ['/processolicitatorio/dashboard/index']],
+            ],
         ],
 
         [
-            'label' => 'Usuário (' . Html::encode(ucwords(strtolower($session['sess_nomeusuario']))) . ')',
+            'label' => '<i class="bi bi-person-circle"></i> ' . Html::encode(ucwords(strtolower($session['sess_nomeusuario']))),
             'items' => [
-                '<li class="dropdown-header">Área Usuário</li>',
-                ['label' => 'Versões Anteriores', 'url' => ['/site/versao']],
-                ['label' => 'Sair', 'url' => 'https://portalsenac.am.senac.br/portal_senac/control_base_vermodulos/control_base_vermodulos.php'],
+                '<li class="dropdown-header text-muted">Área do Usuário</li>',
+                ['label' => '<i class="bi bi-clock-history"></i> Versões Anteriores', 'url' => ['/site/versao']],
+                ['label' => '<i class="bi bi-box-arrow-right"></i> Sair', 'url' => 'https://portalsenac.am.senac.br/portal_senac/control_base_vermodulos/control_base_vermodulos.php'],
             ],
         ],
 
     ] : [
 
-        ['label' => 'Processos Licitatórios', 'url' => ['/processolicitatorio/processo-licitatorio/consulta-processos-licitatorios']]
+        ['label' => '<i class="bi bi-file-text-fill"></i> Processos Licitatórios', 'url' => ['/processolicitatorio/processo-licitatorio/consulta-processos-licitatorios']]
 
     ],
 ]);
